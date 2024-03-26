0e89424ede63100dadb65b3a7722b1b327b5c0e5
Mukhammadali <mukhammadalikamalov@gmail.com>
2024-03-26 12:23:02 +0500

    the best

--- scripts/home.js
+++ scripts/home.js
@@ -9,3 +9,30 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+import createBanner from "./module/banner.js";
+import { pushMovie } from "./module/swiper.js";
+import { getMovies, getTrending, getUpcoming } from "./module/utils.js";
+
+const titles = {
+  0: "Top Rated Movies",
+  1: "popular movies",
+  2: "Top Rated Tv Shows",
+  3: "Popular Tv Shows",
+};
+
+getTrending("all").then(({ results }) => pushMovie(results, 5, null, titles[5]));
+
+getMovies().then((res) => {
+  const [movies, tv] = res;
+  const all = [...movies, ...tv];
+
+  createBanner({ ...movies[0].results[5], type: "movie" });
+
+  all.forEach(({ results }, i) => {
+    const type = i < 2 ? types[0] : types[1];
+    pushMovie(results, i, type, titles[i]);
+  });
+});
+
+getUpcoming().then(({ results }) => pushMovie(results, 4, "movie", titles[4]));
--- scripts/module/banner.js
+++ scripts/module/banner.js
@@ -1,24 +1,60 @@
-const API_KEY = '86783762237ff3e97be67f3473685c59';
-const BASE_PATH = 'https://api.themoviedb.org/3';
+import { createModal, getMovieImage, getTrailerVideo, paintGenres, saveLocalMovies } from "./utils.js";
 
-const getTrailerVideo = async (type, id) => {
-    const response = await fetch(`${BASE_PATH} / ${type} / ${id} / videos ? api_key = ${API_KEY}`);
-    const data = await response.json();
+const banner = document.querySelector(".banner");
+const title = banner.querySelector(".title");
+const overview = banner.querySelector(".overview");
+const playBtn = document.querySelectorAll("#playBtn");
+const infoBtn = document.querySelectorAll("#infoBtn");
+const addBtn = document.querySelectorAll("#addBtn");
 
-    return data.results[0].key;
+export default function createBanner(data) {
+  banner.style.backgroundImage = dynamicBgImage(data.poster_path, data.backdrop_path);
+  title.textContent = `${data.title || data.name}`;
+  overview.textContent = `${data.overview}`;
+
+  document.querySelector(".banner-genres").append(paintGenres(data));
+
+  infoBtn.forEach((info) =>
+    info.addEventListener("click", (e) => {
+      e.preventDefault();
+      createModal(data);
+    })
+  );
+
+  playBtn.forEach((play) =>
+    play.addEventListener("click", (e) => {
+      e.preventDefault();
+      createVideo(data.id, data.type || data.media_type);
+    })
+  );
+
+  addBtn.forEach((add) => add.addEventListener("click", () => saveLocalMovies(data)));
+
+  const mediaQuery = window.matchMedia("(min-width: 714px)");
+  mediaQuery.addEventListener(
+    "change",
+    () => (banner.style.backgroundImage = dynamicBgImage(data.poster_path, data.backdrop_path))
+  );
+}
+
+function dynamicBgImage(poster_path, backdrop_path) {
+  return window.innerWidth < 714
+    ? `linear-gradient(to top, #100f0f 10%, rgba(0, 0, 0, 0) 50%), url(${getMovieImage(poster_path)})`
+    : `linear-gradient(to right, #100f0f 5%, rgba(0, 0, 0, 0) 90%), 
+   url(${getMovieImage(backdrop_path)})`;
+}
+
+const video = document.querySelector("iframe");
+const createVideo = (id, type) => {
+  getTrailerVideo(type, id).then(({ results }) => {
+    const videoId = results.findIndex((v) => v.name.includes("Trailer"));
+    body.classList.add("video-active");
+    video.src = `https://youtube.com/embed/${results[videoId].key}?autoplay=1&mute=1`;
+  });
 };
 
-document.getElementById('playButton').addEventListener('click', async () => {
-    const trailerKey = await getTrailerVideo('movie', '131701');
-    const trailerUrl = `https://www.youtube.com/embed/${trailerKey}`;
-
-    const modal = document.getElementById('modal');
-
-    modal.innerHTML = <iframe width="560" height="315" src="${trailerUrl}" frameborder="0" allowfullscreen></iframe>;
-
-    modal.style.display = 'block';
-});
-
-document.getElementById('closeButton').addEventListener('click', () => {
-    document.getElementById('modal').style.display = 'none';
+const iframeWrapper = document.querySelector(".iframe-wrapper");
+iframeWrapper.addEventListener("click", () => {
+  body.classList.remove("video-active");
+  video.src = "";
 });
